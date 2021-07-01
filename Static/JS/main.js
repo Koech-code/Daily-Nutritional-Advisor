f63f3491c8f20c2d2ad77191c8c231d1c713a8c6
Koech-code <nixon.koech@moringaschool.com>
2021-07-01 13:14:59 +0300

    made changes to the index.html, styles and added javascript functions

--- Static/JS/main.js
+++ Static/JS/main.js
@@ -6,7 +6,9 @@ function bmi(){
     var height=parseFloat(document.getElementById("input1").value);
     var weight=parseFloat(document.getElementById("input2").value);
     var BMI=weight/(height/100*height/100);
-
-    document.getElementById("input3").value=BMI;
-    
+    if(height>200){
+      alert("Your height is too extreme. Please input another value.")
+    } else{
+    document.getElementById("input3").value=BMI.toFixed(2);
+}
 }
